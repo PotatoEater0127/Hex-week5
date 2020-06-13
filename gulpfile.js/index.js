94ee84b7124d1a10e439bbf80b96837915c079c6
Henry Han <potatoeater0127@gmail.com>
2020-06-13 14:11:28 +0800

    (init) set up ejs layout system and adjust browserSync

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -2,39 +2,46 @@ const gulp = require("gulp");
 const $ = require("gulp-load-plugins")();
 const browserSync = require("browser-sync").create();
 const args = require("minimist")(process.argv.slice(2));
+const { watch, series, src, dest } = gulp;
 
 function sass() {
-  return gulp
-    .src("./src/assets/style/sass/**/*.scss")
+  return src("./src/assets/styles/sass/**/*.scss")
     .pipe($.plumber())
     .pipe($.sourcemaps.init())
     .pipe($.sass())
     .pipe($.autoprefixer())
     .pipe($.sourcemaps.write())
-    .pipe(gulp.dest("./dist/assets/style/css"));
-}
-
-function browser() {
-  browserSync.init({
-    server: "./",
-    port: 8080,
-  });
+    .pipe(dest("./dist/assets/styles/css"));
 }
 
 function html() {
-  return gulp
-    .src("./src/views/**/*.html")
+  return src("./src/views/**/*.html")
     .pipe($.plumber())
     .pipe($.frontMatter())
     .pipe($.layout((file) => file.frontMatter))
-    .pipe(gulp.dest("./dist/views"));
+    .pipe(dest("./dist/views"));
+}
+
+function _reload(done) {
+  browserSync.reload();
+  done();
+}
+
+function serve() {
+  browserSync.init({
+    server: { baseDir: "dist", index: "./views/index.html" },
+    port: 8080,
+  });
+
+  watch("src/views/**/*.{html,ejs}", series([html, _reload]));
+  watch("src/assets/styles/**/*.{scss,css}", series([sass, _reload]));
 }
 
 function clean() {
-  return gulp.src("./dist", { read: false, allowEmpty: true }).pipe($.clean());
+  return src("./dist", { read: false, allowEmpty: true }).pipe($.clean());
 }
 
-exports.sass = sass;
-exports.browser = browser;
 exports.html = html;
+exports.sass = sass;
+exports.serve = serve;
 exports.clean = clean;
